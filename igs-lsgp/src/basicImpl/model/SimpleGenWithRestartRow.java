aac1cd526cae7da5ee4eae1228ab096bf81d9d97
Ignacio Gallego Sagastume <bluemontag@gmail.com>
2015-05-08 12:52:18 -0300

    new method for generating LSs

---
+++ igs-lsgp/src/basicImpl/model/SimpleGenWithRestartRow.java
@@ -0,0 +1,109 @@
+/**
+ * Creation date: 08/05/2015
+ * 
+ */
+package basicImpl.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import basicImpl.utils.RandomUtils;
+
+/**
+ * @author igallego
+ *
+ */
+public class SimpleGenWithRestartRow extends SimpleGen {
+	
+	
+	public SimpleGenWithRestartRow(int n) {
+		super(n);
+	}
+	
+//	private HashSet<Integer>[] copyCollection(HashSet<Integer>[] col) {
+//		@SuppressWarnings("unchecked")
+//		HashSet<Integer>[] newCol =  new HashSet[n];
+//		
+//		for(int i=0; i<n; i++) {
+//			newCol[i] = new HashSet<Integer>();
+//			newCol[i].addAll(col[i]);
+//		}
+//		
+//		return newCol;
+//	}
+	
+	@Override
+	public ArrayList<Integer> generateRow(int i_row, int n, LatinSquare ls, HashSet<Integer>[] availableInCol, Integer[] failedAttemptsPerRow, int[][] collisions) {
+		HashSet<Integer> availableInRow = new HashSet<Integer>();
+	    for (int j=0; j<n; j++) {
+	    	availableInRow.add(j);
+	    }
+	    
+	    int colcount = 0;
+	    
+	    //resultado del algoritmo
+	    ArrayList<Integer> row = new ArrayList<Integer>();
+	    int i_col = 0;
+	    
+	    ArrayList<Integer> beginWith = new ArrayList<Integer>();
+	    
+	    while (i_col < n) {//cuando llega a n, elegi n numeros
+	        //conjunto de disponibles es:
+	        HashSet<Integer> available = new HashSet<Integer>();
+	        available.addAll(availableInCol[i_col]);
+	    	available.retainAll(availableInRow);
+	    	
+	    	if (i_col==0) {
+	    		available.removeAll(beginWith);
+	    		if (available.isEmpty()) {
+	    			available.addAll(beginWith);
+	    		}
+	    	}
+	    	
+	        if (!available.isEmpty()) { //si me quedan disponibles
+	            //choose a symbol at random
+	            Integer symbol = RandomUtils.randomChoice(available);
+	            
+	            if (i_col==0) {
+	            	beginWith.add(symbol);
+	            }
+	            
+	            //count the chosen symbol
+	            availableInCol[i_col].remove(symbol);
+	            availableInRow.remove(symbol);
+	            row.add(symbol);
+	            i_col++;
+	        } else {//collision
+	        	colcount++;
+	        	
+	        	//remove all symbols in row and return to available
+	        	while (i_col>0) {
+	        		i_col--;
+	        		Integer symbol = row.get(i_col);
+	        		availableInCol[i_col].add(symbol);
+	        		availableInRow.add(symbol);
+	        		row.remove(i_col);
+	        	}
+//	        	
+//	        	
+//	        	row = new ArrayList<Integer>();
+//	            availableInCol = this.copyCollection(initialColsAvailable);
+//	            
+//	            //return all elements to available in row
+//	            availableInRow = new HashSet<Integer>();
+//	            for (int j=0; j<n; j++) {
+//		    		availableInRow.add(j);
+//	            }
+//	            i_col = 0;
+	            if (colcount%10000==0) {
+	            	System.out.println(colcount +" restarts!");
+	            }
+	        }
+	    }
+	    System.out.println("Row "+i_row+" finished.");
+	    
+	    return row;
+	}
+
+
+}
