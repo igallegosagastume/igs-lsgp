05bd60816bb1dc7e37163c65f956817e58dd750a
Ignacio Gallego Sagastume <bluemontag@gmail.com>
2016-03-09 15:49:09 -0300

    Progress on mckay algorithm

--- igs-lsgp/src/mckaywormald/test/McKayLRGenerationMethod.java
+++ igs-lsgp/src/mckaywormald/test/McKayLRGenerationMethod.java
@@ -6,10 +6,10 @@ package mckaywormald.test;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import mckaywormald.model.LatinRectangle;
-
 import commons.RandomUtils;
 
 /**
@@ -18,19 +18,20 @@ import commons.RandomUtils;
  */
 public class McKayLRGenerationMethod {
 	
+	//auxiliary variables for the algorithm
 	private Set<Integer> initiallyAvInRow = null;
 	private int conflictsCount = 0;
 	private int overlappingConflicts = 0;
+	private Integer[] conflictsPerColumn = null; 
+	private HashSet<Integer>[] availableInCol = null; 
 	
 	public McKayLRGenerationMethod() {
 		RandomUtils.initRand();
 	}
 	
 	public LatinRectangle generateLR(int k, int n) {
-		this.initiallyAvInRow = RandomUtils.oneToN(n);
-		this.conflictsCount = 0;
-		this.overlappingConflicts = 0;
 		
+		this.initiallyAvInRow = RandomUtils.oneToN(n);
 		boolean rejected = false;
 		LatinRectangle a = null;
 		
@@ -49,6 +50,21 @@ public class McKayLRGenerationMethod {
 	}
 	
 	private LatinRectangle randomMemberOfMkn(int k, int n) {
+		this.conflictsCount = 0;
+		this.overlappingConflicts = 0;
+		conflictsPerColumn = new Integer[n]; //tells how many conflicts there are for each column
+		for (int i=0; i<n; i++) {
+			conflictsPerColumn[i] = 0;
+		}
+		
+		availableInCol = new HashSet[n];
+	    for (int i=0; i<n; i++) {//initially all numbers from 1 to n
+	    	availableInCol[i] = new HashSet<Integer>();
+	    	for (int j=0; j<n; j++) {
+	    		availableInCol[i].add(j);
+	    	}
+	    }
+	    
 		LatinRectangle lr = new LatinRectangle(k, n);
 	    
 	    for (int i=0; i<n; i++) {
@@ -69,6 +85,11 @@ public class McKayLRGenerationMethod {
 	            Integer symbol = RandomUtils.randomChoice(availableInRow);
 	            availableInRow.remove(symbol);
 	            row.add(symbol);
+	            
+	            if (!this.availableInCol[j].contains(symbol)) {
+	            	conflictsPerColumn[j]++;
+	            }
+	            
 	            j++;
 	    }
 	    
